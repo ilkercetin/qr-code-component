2291f0fb73124b9bb5a61c4a1ff27dd8db48405a
ilker.cetin <97ilkercetin@gmail.com>
2023-06-20 18:41:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import QrCode from "./components/QrCode.vue";
+import imgUrl from './assets/images/image-qr-code.png';
+</script>
+
+<template>
+    <qr-code :qr-image-link='imgUrl'
+             qr-title="Improve your front-end skills by building projects"
+             qr-description="Scan the QR code to visit Frontend Mentor and take your coding skill to the next level"/>
+</template>
+
+<style>
+    body {
+        background-color: hsl(212, 45%, 89%);
+    }
+</style>
---
+++ src/components/QrCode.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="qr-code-card">
+        <img class="qr-image" :src='qrImageLink'>
+        <div class="qr-title"> {{ qrTitle }}</div>
+        <div class="qr-description"> {{ qrDescription }}</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        qrImageLink: string,
+        qrTitle: string,
+        qrDescription: string
+    }>()
+</script>
+
+<style scoped lang="scss">
+    .qr-code-card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: hsl(0, 0%, 100%);
+        padding: 16px;
+        border-radius: 25px;
+        font-family: Outfit;
+        width: 300px;
+
+        .qr-image {
+            width: 100%;
+            border-radius: 10px;
+        }
+
+        .qr-title {
+            margin: 16px 0;
+            font-size: 24px;
+            font-weight: 700;
+            color: hsl(218, 44%, 22%);
+        }
+
+        .qr-description {
+            width: 80%;
+            font-size: 15px;
+            margin-bottom: 16px;
+            color: hsl(220, 15%, 55%);
+        }
+    }
+</style>
